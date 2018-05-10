1f48b96fbcf6c2f313639fe9795ed29667c932ef
Ryan Wittrup <ryan_wittrup@dell.com>
2018-05-10 10:28:57 -0500

    Fix downloading of chocolatey install script successfully
    Also:
    - make machine-level setup wait for user input on error
    so we can actually see what went wrong in the elevated powershell window.
    - fix userlevel setup within the same install
    
    Signed-off-by: George Gelashvili <ggelashvili@pivotal.io>

--- setup.ps1
+++ setup.ps1
@@ -4,13 +4,17 @@ param([Boolean]$silent=$false)
 $start_path = $pwd.path
 
 Write-Host " * Running machine-level setup..."
+
 $process = Start-Process powershell -Wait -PassThru -Verb runas -ArgumentList "-File $start_path/machine_level_setup.ps1"
 
 Write-Host $process.ExitCode
 If ($process.ExitCode -ne 0) {
-    Write-Warning 'Situation is suboptimal. Machine-level setup failed with error code: $($process.ExitCode)'
+    Write-Warning "Situation is suboptimal. Machine-level setup failed with error code: $($process.ExitCode)"
     break
 }
 
+Write-Host " * Making machine-level PATH changes apply"
+
 Write-Host " * Running user-level setup..."
+Unblock-File user_level_setup.ps1
 ./user_level_setup.ps1
